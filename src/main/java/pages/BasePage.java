ba0b8a655b61109fb6eab3563f808a7baa4e2a29
Junior Alexis Llantén Vélez <junior.velez@fh.co>
2026-05-13 14:31:49 -0500

    new method implmented in the base page and fluent wait modified for being faster

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -2,28 +2,45 @@ package pages;
 
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
 
 import java.time.Duration;
 
-public class BasePage {
+public abstract class BasePage {
 
-    private WebDriver driver;
-    private Wait<WebDriver> wait;
+    protected WebDriver driver;
+    protected Wait<WebDriver> wait;
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
         this.wait = new FluentWait<>(driver)
                 .withTimeout(Duration.ofSeconds(10))
-                .pollingEvery(Duration.ofSeconds(1))
+                .pollingEvery(Duration.ofSeconds(500))
                 .ignoring(NoSuchElementException.class);
         PageFactory.initElements(this.driver, this);
     }
 
     public void navigate(String url){
         this.driver.get(url);
-        this.driver.manage().window().fullscreen();
+        this.driver.manage().window().maximize();
+    }
+
+    protected void waitForVisibility(WebElement element) {
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    protected void click(WebElement element) {
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+        element.click();
+    }
+
+    protected void type(WebElement element, String text) {
+        waitForVisibility(element);
+        element.clear();
+        element.sendKeys(text);
     }
 }
